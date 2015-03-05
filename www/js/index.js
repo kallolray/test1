4026accbd29deac2a3c2dc160ac5060d9a893c14
rayk <rayk@timken.com>
2015-03-05 22:29:40 +0530

    1

--- www/js/index.js
+++ www/js/index.js
@@ -52,4 +52,10 @@ var app = {
 app.initialize();
 function test1(id){
     alert('test' + id);
+    var element = document.getElementById('deviceProperties');
+        element.innerHTML = 'Device Model: '    + device.model    + '<br />' +
+                            'Device Cordova: '  + device.cordova  + '<br />' +
+                            'Device Platform: ' + device.platform + '<br />' +
+                            'Device UUID: '     + device.uuid     + '<br />' +
+                            'Device Version: '  + device.version  + '<br />';
 }
