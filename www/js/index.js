9d889e9c62213350296f712c91d7b9b4651c7b37
rayk <rayk@timken.com>
2015-03-05 23:27:21 +0530

    1

--- www/js/index.js
+++ www/js/index.js
@@ -53,12 +53,13 @@ app.initialize();
 function test1(id){
     showAlert();
     alert('test' + id);
-    var element = document.getElementById('deviceProperties');
+    /* var element = document.getElementById('deviceProperties');
         element.innerHTML = 'Device Model: '    + device.model    + '<br />' +
                             'Device Cordova: '  + device.cordova  + '<br />' +
                             'Device Platform: ' + device.platform + '<br />' +
                             'Device UUID: '     + device.uuid     + '<br />' +
                             'Device Version: '  + device.version  + '<br />';
+    */
 }
 // alert dialog dismissed
 function alertDismissed() {
