94a914b0364bde295ebee1760e96789b733ada12
rayk <rayk@timken.com>
2015-03-06 22:12:38 +0530

    1

--- www/js/index.js
+++ www/js/index.js
@@ -185,3 +185,31 @@ function getPhoto(source) {
 function onFail(message) {
   alert('Failed because: ' + message);
 }
+
+// Called when capture operation is finished
+//
+function captureSuccess(mediaFiles) {
+    var i, len;
+    var element = document.getElementById("captureFile");
+    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+        //uploadFile(mediaFiles[i]);
+        element.innerHTML = "Path : " + mediaFiles[i].fullPath + ", " +
+            "Name : " + mediaFiles[i].name
+
+    }
+}
+
+// Called if something bad happens.
+//
+function captureError(error) {
+    var msg = 'An error occurred during capture: ' + error.code;
+    navigator.notification.alert(msg, null, 'Uh oh!');
+}
+
+// A button will call this function
+//
+function captureAudio() {
+    // Launch device audio recording application,
+    // allowing user to capture up to 2 audio clips
+    navigator.device.capture.captureAudio(captureSuccess, captureError, {limit: 2});
+}
